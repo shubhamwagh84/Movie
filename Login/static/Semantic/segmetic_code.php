edc6bb75d12f6514105a0ce64b2b636c99910c6d
Shubham Wagh <waghshubham84@gmail.com>
2020-10-23 11:47:20 +0530

    Add files via upload

---
+++ Login/static/Semantic/segmetic_code.php
@@ -0,0 +1,5 @@
+<link rel="stylesheet" type="text/css" href="semantic/semantic.min.css">
+<script src="jquery/jquery.js"
+  integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8="
+  crossorigin="anonymous"></script>
+<script src="semantic/semantic.min.js"></script>
